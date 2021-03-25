2433acc05813b61d7bdf7f7e07feeaa462d21c7c
Leen Hazaimeh <leenhazaimeh94@gmail.com>
2021-03-25 15:57:10 +0200

    makes some changes

---
+++ app.js
@@ -0,0 +1,13 @@
+alert("hello ASAC student's")
+
+
+
+ var choose = prompt("what is your favorite social media site facbooc , instgram, githob");
+ if(choose === 'facbook'){
+     document.write('facbooc is good choice');}
+     else if(choose ==='instgram');
+     {
+        document.write('instgram is good choice');
+
+     } 
+
