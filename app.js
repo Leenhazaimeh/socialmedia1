0e2a69cd4907f46ec5d9a59cd250a696f5215870
Leen Hazaimeh <leenhazaimeh94@gmail.com>
2021-03-27 01:06:17 +0300

    add some efficts

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 alert("hello ASAC student's")
+alert("Let's talk about social media ")
 
 
 
- var choose = prompt("what is your favorite social media site facbooc , instgram, githob");
+ var choose = prompt("what is your favorite social media site facbook , instgram, githob");
  if(choose === 'facbook'){
      document.write('facbook is good choice');}
      else if(choose ==='instgram')
@@ -15,4 +16,21 @@ alert("hello ASAC student's")
 
      }
       else {document.write('thanck for your vesiting')}
+
+
+
+      var choose = prompt("how many hours you used it? all the time ,just acoubel of hours , idont use them ");
+ if(choose === 'all the time'){
+     document.write('  you have to decreas the time for social media and focus on your life a little');}
+     else if(choose ==='just acoubel of hours')
+     {
+        document.write('  I think you promoot your time pirfectlty');
+
+     } else if(choose === 'idont use them')
+     {
+         document.write('  you have to tryed one of them');
+
+     }
+      else {document.write('  try to select one of the choices')}
+
 
